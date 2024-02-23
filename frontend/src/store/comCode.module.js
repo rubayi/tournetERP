66d322bb7fc06cf4e017a80fbb104555689c991d
TNSYS03\TNHAWAII03 <rubayi@gmail.com>
2024-02-22 17:14:56 -1000

    수정

---
+++ frontend/src/store/comCode.module.js
@@ -0,0 +1,74 @@
+import CommonCodeService from "../services/comcode.service";
+
+const commCode =
+  { codeUuid:null,
+    codeKr:"",
+    codeEn:"",
+    codeAbb:"",
+    codeValue:"",
+    codeLvl:"",
+    codeOrd:null,
+    uprCodeUuid:null,
+    useYn:"",
+    etc:""
+  };
+const initialState = commCode
+  ? { message: "", commCode }
+  : { message: "", commCode: null };
+
+export const comCode = {
+  namespaced: true,
+  state: initialState,
+  actions: {
+    useComCode({ commit }, comreq) {
+      return CommonCodeService.useComCode(comreq).then(
+        data => {
+          commit('searchCommonCode', data);
+          return Promise.resolve(data);
+        },
+        error => {
+          commit('comCodeError');
+          return Promise.reject(error);
+        }
+      );
+    },
+    deleteComcode({ commit }, id) {
+      return CommonCodeService.deleteComcode(id).then(
+        data => {
+          commit('deleteCommonCode', data);
+          return Promise.resolve(data);
+        },
+        error => {
+          commit('comCodeError');
+          return Promise.reject(error);
+        }
+      );
+
+    },
+    createComcode({ commit }, comreq) {
+      return CommonCodeService.createComcode(id).then(
+        data => {
+          commit('createSuccess', data);
+          return Promise.resolve(data);
+        },
+        error => {
+          commit('comCodeError');
+          return Promise.reject(error);
+        }
+      );
+    }
+  },
+  mutations: {
+    searchCommonCode(state, data) {
+      state.comCode = data;
+
+    },
+    deleteCommonCode(state) {
+      state.success = true;
+    },
+    createSuccess(state) {
+    },
+    comCodeError(state) {
+    },
+  }
+};
