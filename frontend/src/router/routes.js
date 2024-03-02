def97761453a5d7d9ab7494b0b2693cbb3cc4363
Youngjun Kim <youngjunkim119@gmail.com>
2024-03-01 16:25:28 -1000

    Update routes.js

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,7 +1,7 @@
 import Home from "components/Homehome.vue";
 import Login from "components/LoginTN.vue";
 import Register from "components/RegisterTN.vue";
-import Comcode from "components/Comcode.vue";
+import Comcode from "components/ComCode.vue";
 
 const Profile = () => import("../components/Profile.vue")
 const BoardAdmin = () => import("../components/BoardAdmin.vue")
